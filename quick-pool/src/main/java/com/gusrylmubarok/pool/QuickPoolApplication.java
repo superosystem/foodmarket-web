d4b9c140b00e09d1463c40342b95cd704e3c5018
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-12-29 14:03:29 +0700

    Rewrite

---
+++ quick-pool/src/main/java/com/gusrylmubarok/pool/QuickPoolApplication.java
@@ -0,0 +1,12 @@
+package com.gusrylmubarok.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickPoolApplication.class, args);
+    }
+
+}
---
+++ quick-pool/src/test/java/com/gusrylmubarok/pool/QuickPoolApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gusrylmubarok.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
